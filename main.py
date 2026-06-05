14c008aa0ad3a8a98941692aafbfdfa59f61df33
sudo-nikhil <nikhilkabaka2004@gmail.com>
2026-06-05 12:30:12 +0530

    cahnged something

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!!"
+
+@app.route("/abc")
+def abc():
+    return "This is the ABC route"
+
+@app.route("/<name>")
+def greet(name):
+    return f"Hello , {name}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
